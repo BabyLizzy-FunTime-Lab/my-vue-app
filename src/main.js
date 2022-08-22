7784ad98351041dcf7baefbdf2e2e34101d3e91f
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2022-08-22 13:39:00 +0200

    fixing conflict

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+// Bootsrtap styling
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 createApp(App).mount('#app')
