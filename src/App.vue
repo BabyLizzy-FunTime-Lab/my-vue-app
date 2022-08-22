437456ac02fe8a048df7c1babd55c35b8b34676d
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2022-08-22 17:24:12 +0200

    binding data to vacationpicker app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="This is my first Vue-app!!"/>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">VacationPicker</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+  </nav>
+  <div class="jumbotron">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+  <VacationPicker />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VacationPicker from './components/VacationPicker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VacationPicker
   }
 }
 </script>
 
-<!-- <style>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style> -->
+</style>
+<style scoped>
+  .jumbotron {
+    text-align: center;
+
+  }
+  nav {
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+</style>
---
+++ src/components/VacationPicker.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <h1>Pick your next vacation.</h1>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="country in countryData.countries" v-bind:key="country.id">
+                {{country.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import countryData from '@/data/countryData';
+    export default {
+        name: 'VacationPicker',
+        data: function() {
+            return {
+                countryData: countryData
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
