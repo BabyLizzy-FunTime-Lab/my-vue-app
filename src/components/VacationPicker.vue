3cfa8d7d05568937586a8573bb601cdc2c776db4
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2022-08-25 16:18:29 +0200

    binding data and beginning with methods

--- src/components/VacationPicker.vue
+++ src/components/VacationPicker.vue
@@ -1,13 +1,22 @@
 <template>
-    <div class="container">
-        <h1>{{header}}</h1>
-        <ul class="list-group">
-            <li class="list-group-item" v-for="(country, index) in countryData.countries" v-bind:key="country.id"
-                v-bind:title="country.details">
-                {{index}} -
-                {{country.name}}
-            </li>
-        </ul>
+    <div class="container-fluid">
+        <div class="container">
+            <h1>{{header}}</h1>
+            <ul class="list-group">
+                <li class="list-group-item" v-for="(country, index) in countryData.countries" v-bind:key="country.id"
+                    v-bind:title="country.details">
+                    {{index}} -
+                    {{country.name}}
+                </li>
+            </ul>
+        </div>
+        <div class="container teller">
+            <h3>Teller: {{counter}}</h3>
+            <button v-on:click="counter++" class="btn btn-success">+</button>
+            <button v-on:click="counter--" class="btn btn-danger">-</button>
+        </div>
+        
+        
     </div>
 </template>
 
@@ -18,12 +27,16 @@
         data: function() {
             return {
                 countryData: countryData,
-                header: 'Vue Vacation Picker'
+                header: 'Vue Vacation Picker',
+                counter: 0
             }
         }
     }
 </script>
 
-<style>
-
+<style scoped>
+    .teller {
+        text-align: center;
+        padding: 1em 0em 1em 0em;
+    }
 </style>
